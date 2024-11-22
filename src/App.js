4a2ac2e2c549d0c8003b6642561fb210ccb09f57
Hema <hema391@gmail.com>
2024-11-22 16:17:18 +0530

    ProductDetails page committed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
 import Product from "./customer/components/Product/Product";
 import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
+import Cart from "./customer/components/Cart/Cart";
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
       <div className="relative z-50">
         <Navigation />
       </div>
-      <div>
+      <div className="my-10">
         {/* <HomePage /> */}
         {/* <Product /> */}
-        <ProductDetails />
+        {/* <ProductDetails /> */}
+        <Cart />
       </div>
 
       <Footer />
---
+++ src/customer/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className="p-5 shadow-lg border rounded-md">
+      <div className="flex items-center">
+        <div className="w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]">
+          <img
+            className="w-full h-full object-cover object-top"
+            src=""
+            alt=""
+          />
+        </div>
+        <div className="ml-5 space-y-1">
+          <p className="font-semibold">Men Slim Mid Rise Black Jeans</p>
+          <p className="opacity-70">Size: L, Wihte</p>
+          <p className="opacity-70 mt-2">Seller: Crishtaliyo 2Fashion</p>
+          <div className="flex space-x-5 items-center text-gray-900 pt-6">
+            <p className="font-semibold">₹199</p>
+            <p className="opacity-50 line-through">₹211</p>
+            <p className="text-green-600 font-semibold">5% off</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
